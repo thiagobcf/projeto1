0c9b13346218b06f34b85f0ba48f9079920c4bc5
thiagobcf <thiagobcfonseca@gmail.com>
2023-08-08 16:24:47 -0300

    test2

--- authors/tests/test_author_register_for.py
+++ authors/tests/test_author_register_for.py
@@ -24,9 +24,9 @@ class AuthorRegisterFormUnitTest(TestCase):
             'Letras, números e @/./+/-/_ apenas.')),
         ('email', 'The e-mail must be valid.'),
         ('password', (
-            'Password must have at least one uppercase letter, '
-            'one lowercase letter and one number. The length should be '
-            'at least 8 characters.'
+            'Password must have at least one uppercase letter'
+
+
         )),
     ])
     def test_fields_help_text(self, field, needed):
