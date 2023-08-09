af1f1d3e70cf84a7481520aa977c77bb9c46b5cb
thiagobcf <thiagobcfonseca@gmail.com>
2023-08-09 19:39:11 -0300

    re-ordem

--- authors/forms.py
+++ authors/forms.py
@@ -100,7 +100,8 @@ class RegisterForm(forms.ModelForm):
 
         if exists:
             raise ValidationError(
-                'User e-mail is already in use', code='invalid')
+                'User e-mail is already in use', code='invalid',
+            )
 
         return email
 
