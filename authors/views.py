b24a7068b59c45c3427711ef6741617365310950
thiagobcf <thiagobcfonseca@gmail.com>
2023-08-10 11:50:09 -0300

    login, url, views, template

--- authors/views.py
+++ authors/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
+from django.urls import reverse
 
 from .forms import RegisterForm
 
@@ -10,6 +11,7 @@ def register_view(request):
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
+        'form_action': reverse('authors:register_create'),
     })
 
 
@@ -30,3 +32,11 @@ def register_create(request):
         del (request.session['register_form_data'])
 
     return redirect('authors:register')
+
+
+def login_view(request):
+    return render(request, 'authors/pages/login.html')
+
+
+def login_create(request):
+    return render(request, 'authors/pages/login.html')
