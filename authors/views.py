8fb144f7a1677ebaef1594d5d9b86ca1e1e2dfbd
thiagobcf <thiagobcfonseca@gmail.com>
2023-08-17 12:56:27 -0300

    add recipe_form

--- authors/views.py
+++ authors/views.py
@@ -5,6 +5,7 @@ from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from authors.forms.recipe_form import AuthorRecipeForm
 from recipes.models import Recipe
 
 from .forms import LoginForm, RegisterForm
@@ -106,14 +107,20 @@ def dashboard_recipe_edit(request, id):
         is_published=False,
         author=request.user,
         pk=id
-    )
+    ).first()
+
     if not recipe:
         raise Http404()
+
+    form = AuthorRecipeForm(
+        data=request.POST or None,
+        instance=recipe
+    )
 
     return render(
         request,
         'authors/pages/dashboard_recipe.html',
         context={
-
+            'form': form
         }
     )
